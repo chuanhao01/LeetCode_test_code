3c3bcc18e8cf092aea7383aadb35b1dabe41d61f
chuanhao01 <chuanhaolim@gmail.com>
2019-03-17 19:16:54 +0800

    will append all items in linked list regardless of length

--- Merge Two Sorted Lists.py
+++ Merge Two Sorted Lists.py
@@ -7,20 +7,22 @@
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
         self.checkMerge = True
+        self.l1check = False
+        self.l2check = False
         self.list = []
         self.templ1 = l1
         self.templ2 = l2
         while(self.checkMerge):
-            try:
-                self.list.append(self.templ1.val)
-            except:
-                pass
-            try:
-                self.list.append(self.templ2.val)
-            except:
-                pass
-            if (self.templ1.next == None and self.templ2.next == None):
+            self.list.append(self.templ1.val)
+            self.list.append(self.templ2.val)
+            if (self.templ1.next != None):
+                self.templ1 = self.templ1.next
+            elif (self.templ1.next == None):
+                self.l1check = True
+            if (self.templ2.next != None):
+                self.templ2 = self.templ2.next
+            elif (self.templ2.next == None):
+                self.l2check = True
+            if (self.l1check and self.l2check):
                 self.checkMerge = False
-            self.templ1 = self.templ1.next
-            self.templ2 = self.templ2.next
         print(self.list)
