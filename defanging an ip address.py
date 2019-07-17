94a64f28f46fd389b9ef36e99e5ef3ae63930371
chuanhao01 <chuanhaolim@gmail.com>
2019-07-18 00:25:34 +0800

    finished another leet code question

--- defanging an ip address.py
+++ defanging an ip address.py
@@ -1,10 +1,17 @@
 class Solution:
-    def defangIPaddr(self, address):
-        address = address.split(".")
-        new_addr = ''
-        for i in range(len(address)):
-            if(i<len(address) - 1):
-                new_addr += address[i] + '[.]'
-            else:
-                new_addr += address[i]
-        return new_addr
+    def numJewelsInStones(self, J, S):
+        counter_jewels = 0
+        jewels = 0
+        new_arr = []
+        for i in S:
+            new_arr.append(i)
+        S = new_arr
+        while(jewels<len(J)):
+            stone = len(S) - 1
+            while(stone > -1):
+                if(S[stone] == J[jewels]):
+                    counter_jewels += 1
+                    S.pop(stone)
+                stone -= 1
+            jewels += 1
+        return counter_jewels
