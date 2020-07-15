06573f88dc0a8d127da622b0a2a18183cc64fffa
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2020-07-16 00:31:05 +0800

    some algo stuff

---
+++ Algos/knapsack/algo.js
@@ -0,0 +1,13 @@
+const algo = {
+    recur(weights, values, maxWeight){
+        boo = [];
+        for(let i of values){
+            boo.push(0);
+        }
+        function r(i, v){
+
+        }
+    }
+};
+
+module.export = algo;
---
+++ Algos/knapsack/test.js
@@ -0,0 +1,7 @@
+const algo = require('./algo');
+
+describe('Test algo', function(){
+    it('Basic', function(){
+        
+    });
+});
