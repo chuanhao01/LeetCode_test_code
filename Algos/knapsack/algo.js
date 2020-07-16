8bc214b140932e140bdabd74ca354782d7607e29
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2020-07-16 21:14:47 +0800

    working knapsack algo

--- Algos/knapsack/algo.js
+++ Algos/knapsack/algo.js
@@ -1,13 +1,48 @@
 const algo = {
     recur(weights, values, maxWeight){
-        boo = [];
+        var boo = [];
         for(let i of values){
             boo.push(0);
         }
-        function r(i, v){
+        // Own implementation
+        // function re(i, c){
+        //     if(i === -1 || c === 0){
+        //         // Base case
+        //         return 0;
+        //     }
+        //     else if(weights[i] > c){
+        //         return re(i-1, c);
+        //     }
+        //     else{
+        //         let temp1 = re(i-1, c);
+        //         let temp2 = values[i] + re(i-1, c - weights[i]);
+        //         if(temp2 > temp1){
+        //             boo[i] = 1;
+        //         }
+        //         return Math.max(temp1, temp2);
+        //     }
+        // }
+        // return re(weights.length - 1, maxWeight);
 
+        // Better
+        function re(i, c){
+            if(i === 0 || c === 0){
+                return 0;
+            }
+            else if(weights[i-1] > c){
+                return re(i-1, c);
+            }
+            else{
+                let temp1 = re(i-1, c);
+                let temp2 = values[i-1] + re(i-1, c - weights[i-1]);
+                if(temp2 > temp1){
+                    boo[i-1] = 1;
+                }
+                return Math.max(temp1, temp2);
+            }
         }
+        return re(weights.length - 1, maxWeight);
     }
 };
 
-module.export = algo;
+module.exports = algo;
--- Algos/knapsack/test.js
+++ Algos/knapsack/test.js
@@ -1,7 +1,12 @@
-const algo = require('./algo');
+const a = require('./algo');
+const expect = require('chai').expect;
 
 describe('Test algo', function(){
     it('Basic', function(){
-        
+        let w = [1, 2, 4, 2, 5];
+        let v = [5, 3, 5, 3, 2];
+        let mw = 10;
+        let mv = a.recur(w, v, mw);
+        expect(mv).to.be.equal(16);
     });
 });
