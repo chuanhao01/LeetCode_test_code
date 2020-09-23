d3a863947dd5b7e5820c24270b8c06b8b3d36868
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2020-09-23 23:17:27 +0800

    solved rock band

--- Kattis/cpp/rockband/main.cpp
+++ Kattis/cpp/rockband/main.cpp
@@ -1,25 +1,39 @@
 #include <iostream>
 #include <vector>
 #include <unordered_set>
-#include <unorder_map>
+#include <unordered_map>
 
 int main(){
     int m, s;
-    int pref;
-    std::unorder_map<int, int> prefs;
     std::cin >> m >> s;
+    std::vector<std::vector<int>> prefs(m+1, std::vector<int>(s+1, 0));
+    // Reading in stdin
     for(int i=0; i<m; i++){
-        for(int k=0; j<s; j++){
-            std::cin >> pref;
-            if(prefs.count(pref) == 0){
-                // Does not exists
-                prefs[pref] = k;
-            }
-            else{
-                prefs[pref] += k;
-            }
+        for(int j=0; j<s; j++){
+            std::cin >> prefs[i][j];
         }
     }
+    int done = 0;
+    std::vector<int> seen(s+1);
+    for(int j=0; j<s; j++){
+        // Adding 1 to song that is seen
+        for(int i=0; i<m; i++){
+            seen[prefs[i][j]]++;
+            if(seen[prefs[i][j]] == m){
+                done++;
+            }
+        }
+        if(done == (j+1)){
+            std::cout << j+1 << std::endl;
+            for(int ij=0; ij<s; ij++){
+                if(seen[ij+1] == m){
+                    std::cout << ij+1 << ' ';
+                }
+            }
+            return 0;
+        }
+    }
+    return 0;
 }
 
 
