a1c7d7f4ddc11d97b9a026f2e6d787b42948162d
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2020-09-27 02:15:24 +0800

    improving template

--- Kattis/cpp/template.cpp
+++ Kattis/cpp/template.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <vector>
 #include <unordered_set>
+#include <unordered_map>
+#include <string>
+#include <stack>
+#include <queue>
 
 int main(){
-
+    return 0;
 }
 
 
