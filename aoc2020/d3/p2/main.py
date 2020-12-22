0df8047a6da76d2366975d3bbf4e551ca00d3c65
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2020-12-22 15:53:40 +0800

    done with d3 p2

--- aoc2020/d3/p2/main.py
+++ aoc2020/d3/p2/main.py
@@ -1,3 +1,15 @@
+def get_count(data, x_cap, y_cap, x_add, y_add):
+    x = 0
+    y = 0
+    count = 0
+    while y < y_cap:
+        if data[y][x] == '#':
+            count += 1
+        x += x_add
+        x %= x_cap
+        y += y_add
+    return count
+
 with open('data.txt') as f:
     x_cap = 0
     y_cap = 0
@@ -8,14 +20,21 @@ with open('data.txt') as f:
         data.append(l)
     y_cap = len(data)
     print(x_cap, y_cap)
-    x = 0
-    y = 0
-    count = 0
-    while y < y_cap:
-        if data[y][x] == '#':
-            count += 1
-        x += 3
-        x %= x_cap
-        y += 1
-    print(count)
+    # x = 0
+    # y = 0
+    # count = 0
+    # while y < y_cap:
+    #     if data[y][x] == '#':
+    #         count += 1
+    #     x += 3
+    #     x %= x_cap
+    #     y += 1
+    # print(count)
+    prod = 1
+    prod *= get_count(data, x_cap, y_cap, 1, 1)
+    prod *= get_count(data, x_cap, y_cap, 3, 1)
+    prod *= get_count(data, x_cap, y_cap, 5, 1)
+    prod *= get_count(data, x_cap, y_cap, 7, 1)
+    prod *= get_count(data, x_cap, y_cap, 1, 2)
+    print(prod)
 
