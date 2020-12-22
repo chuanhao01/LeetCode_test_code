63f3a93064894d747384832c96af154fa2c91be8
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2020-12-22 15:44:11 +0800

    done with d2 p2

--- aoc2020/d2/p2/main.py
+++ aoc2020/d2/p2/main.py
@@ -4,14 +4,14 @@ with open('data.txt') as f:
         l = l.split(': ')
         context = l[0].split(' ')
         c_range = context[0].split('-')
-        c_min = int(c_range[0])
-        c_max = int(c_range[1])
+        c_min = int(c_range[0]) - 1
+        c_max = int(c_range[1]) - 1
         c_word = context[1]
         password = l[1].rstrip()
         count = 0
-        for c in password:
-            if c == c_word:
+        for i in [c_min, c_max]:
+            if password[i] == c_word:
                 count += 1
-        if c_min <= count <= c_max:
+        if count == 1:
             val += 1
 print(val)
