e34e307120eeeeb4c3db38a60f496a8350e96ba0
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2020-12-28 23:41:53 +0800

    done with d18 d1
    Got help online haha
    I can't do parsers because i have not learned them

---
+++ aoc2020/d18/p1/main.py
@@ -0,0 +1,30 @@
+class Num:
+    def __init__(self, num):
+        self.num = num
+
+    def __add__(self, other):
+        return Num(self.num + other.num)
+    
+    def __sub__(self, other):
+        return Num(self.num * other.num)
+    
+    
+
+def custom_eval(l):
+    r_l = ''
+    for c in l:
+        if c in '1234567890':
+            r_l = r_l + f"Num({c})"
+        elif c in '*':
+            r_l = r_l + '-'
+        else:
+            r_l = r_l + c
+    return eval(r_l).num
+
+acc = 0
+with open('data.txt') as f:
+    for l in f:
+        l = l.rstrip()
+        acc += custom_eval(l)
+
+print(acc)
