42f6c2d8fca19e499248d35ae05a100dc58cf0de
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2022-01-11 00:01:27 +0800

    [solve] done with d2

--- aoc2021/d2/p1.py
+++ aoc2021/d2/p1.py
@@ -2,13 +2,26 @@ import typing as t
 
 
 def solve(data: t.Any) -> t.Any:
-    return
+    f = 0
+    d = 0
+    for dd in data:
+        s = dd[0]
+        v = dd[1]
+        if s == "forward":
+            f += v
+        if s == "up":
+            d -= v
+        if s == "down":
+            d += v
+    return f * d
 
 
 def read_data(data_path: str = "data.txt") -> t.Any:
     with open(data_path, "r", encoding="utf-8") as f:
         data = f.read()
-    return data
+    parsed_data = [d.split(" ") for d in data.split("\n")]
+    parsed_data = [[d[0], int(d[1])] for d in parsed_data]
+    return parsed_data
 
 
 if __name__ == "__main__":
--- aoc2021/d2/p2.py
+++ aoc2021/d2/p2.py
@@ -2,13 +2,28 @@ import typing as t
 
 
 def solve(data: t.Any) -> t.Any:
-    return
+    f = 0
+    d = 0
+    a = 0
+    for dd in data:
+        s = dd[0]
+        v = dd[1]
+        if s == "forward":
+            f += v
+            d += a * v
+        if s == "up":
+            a -= v
+        if s == "down":
+            a += v
+    return f * d
 
 
 def read_data(data_path: str = "data.txt") -> t.Any:
     with open(data_path, "r", encoding="utf-8") as f:
         data = f.read()
-    return data
+    parsed_data = [d.split(" ") for d in data.split("\n")]
+    parsed_data = [[d[0], int(d[1])] for d in parsed_data]
+    return parsed_data
 
 
 if __name__ == "__main__":
