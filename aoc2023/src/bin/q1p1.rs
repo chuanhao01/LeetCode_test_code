ed7a2ee0511ec17dbb0f0f2b7da22ee18cde4c11
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2023-12-04 16:18:41 +0800

    [ref] cargo fmt and 4 spaces

--- aoc2023/src/bin/q1p1.rs
+++ aoc2023/src/bin/q1p1.rs
@@ -1,22 +1,27 @@
-use std::{fs::File, io::{Read, Result}};
+use std::{
+    fs::File,
+    io::{Read, Result},
+};
 
-fn main() -> Result<()>{
-  let mut file_input = File::open("inputs/q1")?;
-  let mut input = String::new();
-  file_input.read_to_string(&mut input)?;
+fn main() -> Result<()> {
+    let mut file_input = File::open("inputs/q1")?;
+    let mut input = String::new();
+    file_input.read_to_string(&mut input)?;
 
-  let mut sum = 0;
-  for l in input.split('\n'){
-    let mut digits: Vec<char> = Vec::new();
+    let mut sum = 0;
+    for l in input.split('\n') {
+        let mut digits: Vec<char> = Vec::new();
 
-    let l = l.to_string();
-    for c in l.chars(){
-      if c.is_ascii_digit(){
-        digits.push(c);
-      }
+        let l = l.to_string();
+        for c in l.chars() {
+            if c.is_ascii_digit() {
+                digits.push(c);
+            }
+        }
+        sum += format!("{}{}", digits.first().unwrap(), digits.last().unwrap())
+            .parse::<i64>()
+            .unwrap();
     }
-    sum += format!("{}{}", digits.first().unwrap(), digits.last().unwrap()).parse::<i64>().unwrap();
-  }
-  println!("sum: {}", sum);
-  Ok(())
+    println!("sum: {}", sum);
+    Ok(())
 }
--- aoc2023/src/bin/q1p2.rs
+++ aoc2023/src/bin/q1p2.rs
@@ -1,34 +1,39 @@
-use std::{fs::File, io::{Read, Result}};
+use std::{
+    fs::File,
+    io::{Read, Result},
+};
 
-fn main() -> Result<()>{
-  let mut file_input = File::open("inputs/q1")?;
-  // let mut file_input = File::open("inputs/temp")?;
-  let mut input = String::new();
-  file_input.read_to_string(&mut input)?;
+fn main() -> Result<()> {
+    let mut file_input = File::open("inputs/q1")?;
+    // let mut file_input = File::open("inputs/temp")?;
+    let mut input = String::new();
+    file_input.read_to_string(&mut input)?;
 
-  let mut sum = 0;
+    let mut sum = 0;
 
-  input = input.replace("one", "o1e");
-  input = input.replace("two", "t2o");
-  input = input.replace("three", "t3e");
-  input = input.replace("four", "f4r");
-  input = input.replace("five", "f5e");
-  input = input.replace("six", "s6x");
-  input = input.replace("seven", "s7n");
-  input = input.replace("eight", "e8t");
-  input = input.replace("nine", "n9e");
-  for l in input.split('\n'){
-    let mut digits: Vec<char> = Vec::new();
+    input = input.replace("one", "o1e");
+    input = input.replace("two", "t2o");
+    input = input.replace("three", "t3e");
+    input = input.replace("four", "f4r");
+    input = input.replace("five", "f5e");
+    input = input.replace("six", "s6x");
+    input = input.replace("seven", "s7n");
+    input = input.replace("eight", "e8t");
+    input = input.replace("nine", "n9e");
+    for l in input.split('\n') {
+        let mut digits: Vec<char> = Vec::new();
 
-    let l = l.to_string();
-    for c in l.chars(){
-      if c.is_ascii_digit(){
-        digits.push(c);
-      }
+        let l = l.to_string();
+        for c in l.chars() {
+            if c.is_ascii_digit() {
+                digits.push(c);
+            }
+        }
+        // println!("{}{}", digits.first().unwrap(), digits.last().unwrap());
+        sum += format!("{}{}", digits.first().unwrap(), digits.last().unwrap())
+            .parse::<i64>()
+            .unwrap();
     }
-    // println!("{}{}", digits.first().unwrap(), digits.last().unwrap());
-    sum += format!("{}{}", digits.first().unwrap(), digits.last().unwrap()).parse::<i64>().unwrap();
-  }
-  println!("sum: {}", sum);
-  Ok(())
+    println!("sum: {}", sum);
+    Ok(())
 }
